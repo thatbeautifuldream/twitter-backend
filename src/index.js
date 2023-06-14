8c17c40d5537770b68236b2c594d4c24345de55e
Milind Mishra <milind.mishra4@gmail.com>
2023-06-15 00:36:26 +0800

    📦 NEW: hashtag model updated

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import * as dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import { connect } from "./config/database.js";
-import { Tweet } from "./models/tweet.js";
+// import { Tweet } from "./models/tweet.js";
+import { Hashtag } from "./models/hashtag.js";
 
+dotenv.config();
 const app = express();
 
 app.listen(3000, async () => {
   console.log("Server listening on port 3000");
   connect();
   console.log("Connected to database");
-  Tweet.create({
-    content: "This is my first tweet",
-    likes: 12,
-    noOfRetweets: 2,
-    comment: "This is my first comment",
+
+  // Tweet.create({
+  //   content: "This is my first tweet",
+  //   likes: 12,
+  //   noOfRetweets: 2,
+  //   comment: "This is my first comment",
+  // });
+
+  Hashtag.create({
+    text: "myfirsthashtag",
+    tweets: ["6489ebfc9b67487d2e71eb25"],
   });
 });
---
+++ src/models/hashtag.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const hashtagSchema = new Schema({
+  text: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  tweets: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Tweet",
+    },
+  ],
+});
+
+const Hashtag = mongoose.model("Hashtag", hashtagSchema);
+
+export { Hashtag };
